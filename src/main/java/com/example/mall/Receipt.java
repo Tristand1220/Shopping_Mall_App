c77f25d09beee11f0414aa535bc6069bdb4cf74f
habeeb0502 <143745694+habeeb0502@users.noreply.github.com>
2023-11-07 15:38:23 -0600

    Update Receipt.java

--- src/main/java/com/example/mall/Receipt.java
+++ src/main/java/com/example/mall/Receipt.java
@@ -1,6 +1,50 @@
 package com.example.mall;
 
 public class Receipt {
+    private string itemName;
+    private int itemQuantity;
+    private int itemPrice; 
+    private int totalPrice;
+   
+    // Constructor
+    public Receipt(string itemName, int itemQuantity, int itemPrice, int totalPrice ) {
+        this.itemName = itemName;
+        this.itemQuantity = itemQuantity;
+        this.itemPrice = itemPrice;
+        this.totalPrice = totalPrice;
+    }
+
+    public String getitemName() {
+        return itemName;
+    }
+
+    public void setitemName( string itemName) {
+        this.itemName = itemName; 
+    }
+
+    public int getitemQuantity() {
+        return itemQuantity;
+    }
+  
+    public void setitemQuantity(int itemQuantity) {
+      this.itemQuantity = itemQuantity;
+    }
+
+    public int getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(int totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public int getItemPrice() {
+      return itemPrice;
+    }
+  
+    public void setItemPrice(int itemPrice) {
+      this.itemPrice = itemPrice;
+    }
     public void Isprocessed(String customerEmail, Purchase purchase) {
         // Gather information required for the receipt
         String receiptContent = receipt(purchase);
@@ -25,4 +69,5 @@ public class Receipt {
         System.out.println("Subject: " + subject);
         System.out.println("Content: \n" + content);
     }
+}
 }
